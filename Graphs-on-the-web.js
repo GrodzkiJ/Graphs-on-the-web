562169af976b4814bf6c7ffa25f69c1a2ed28220
GrodzkiJ <jan.grodzki98@gmail.com>
2021-11-21 18:41:44 +0100

    Gituwa

--- Graphs-on-the-web.js
+++ Graphs-on-the-web.js
@@ -6,3 +6,23 @@ function getValue(nazwa) {
 function setTitle(nazwa, tytul) {
     document.getElementById(nazwa).innerHTML = tytul;
 }
+
+function lineChart() {
+    const CHART = document.getElementById("myChart");
+    const labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    let lineChart = new Chart(CHART, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'My First Dataset',
+                    data: [65, 59, 80, 81, 56, 55, 40, 50],
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                }
+            ]
+        }
+    });
+}
